34fc96d1b86bd5b344fb1874e1600c3d3fba0343
John Lombard <John@Johns-MBP.fios-router.home>
2019-01-24 13:51:02 -0500

    dirname

---
+++ client/build/precache-manifest.a4238dabb6ee5e4fd3ab7b8163cd4b97.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "868af9ee95ef411e364265be9884b97b",
+    "url": "/static/media/portfolio.868af9ee.jpg"
+  },
+  {
+    "revision": "9966c33c823658e14f7c6df8b951891c",
+    "url": "/static/media/nyc.9966c33c.jpg"
+  },
+  {
+    "revision": "80b86aa99f32ce80b3b39ab471c03639",
+    "url": "/static/media/budget.80b86aa9.jpg"
+  },
+  {
+    "revision": "cb20c9429ef03f749c11f17f7460c3e8",
+    "url": "/static/media/ben.cb20c942.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "949a92cca5ca351497c8",
+    "url": "/static/js/main.949a92cc.chunk.js"
+  },
+  {
+    "revision": "ec105053da24d0b4c15b",
+    "url": "/static/js/1.ec105053.chunk.js"
+  },
+  {
+    "revision": "949a92cca5ca351497c8",
+    "url": "/static/css/main.d82cb65d.chunk.css"
+  },
+  {
+    "revision": "ec105053da24d0b4c15b",
+    "url": "/static/css/1.290fff62.chunk.css"
+  },
+  {
+    "revision": "12a8fdb663e27261cee1307cd809d013",
+    "url": "/index.html"
+  }
+];
